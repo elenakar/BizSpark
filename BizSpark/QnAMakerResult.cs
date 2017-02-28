dc3984b80b3128a0ed93d8e9517da87be26e1261
b-elkard <b-elkard@B-ELKARD1>
2017-02-28 14:02:14 +0200

    Add project files.

---
+++ BizSpark/QnAMakerResult.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizSpark
+{
+    public class QnAMakerResult
+    {
+        ///
+ 
+        /// The top answer found in the QnA Service.
+        /// </summary>
+ 
+ 
+        [JsonProperty(PropertyName = "answer")]
+        public string Answer { get; set; }
+ 
+        ///
+ 
+        /// The score in range [0, 100] corresponding to the top answer found in the QnA    Service.
+        /// </summary>
+ 
+ 
+        [JsonProperty(PropertyName = "score")]
+        public double Score { get; set; }
+    }
+}
---
+++ BizSpark/SimpleDialog.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BizSpark
+{
+    [Serializable]
+    public class SimpleDialog : IDialog
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(ActivityReceivedAsync);
+        }
+
+        private async Task ActivityReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            var reply2 = activity.CreateReply();
+            reply2.Attachments = new List<Attachment>();
+
+            if (activity.Text.Contains("how many teams"))
+            {
+
+                await context.PostAsync($"There are teams.");
+            }
+            else if (activity.Text.StartsWith("give me"))
+            {
+
+
+                reply2.Attachments.Add(new Attachment()
+                {
+                    ContentUrl = "http://intelligentlabs.co.uk/images/about/qualitycontrol.png",
+                    ContentType = "image/png",
+                    Name = "QualityControl.png"
+                });
+                await context.PostAsync(reply2);
+                context.Wait(ActivityReceivedAsync);
+            
+           }
+            else
+            {
+
+                if (activity.Type == ActivityTypes.Message)
+                {
+                    ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                    var responseString = String.Empty;
+                    var responseMsg = "";
+
+                    //De-serialize the response
+                    QnAMakerResult QnAresponse;
+
+                    // Send question to API QnA bot
+                    if (activity.Text.Length > 0)
+                    {
+                        var knowledgebaseId = "e7e838d2-35a5-4cb1-83ef-c8fc1f91bb8f"; // Use knowledge base id created.
+                        var qnamakerSubscriptionKey = "ee9e28c9d0204de788f504dbe3d5448a"; //Use subscription key assigned to you.
+
+                        //Build the URI
+                        Uri qnamakerUriBase = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
+                        var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+                        //Add the question as part of the body
+                        var postBody = $"{{\"question\": \"{activity.Text}\"}}";
+
+                        //Send the POST request
+                        using (WebClient client = new WebClient())
+                        {
+                            //Set the encoding to UTF8
+                            client.Encoding = System.Text.Encoding.UTF8;
+
+                            //Add the subscription key header
+                            client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                            client.Headers.Add("Content-Type", "application/json");
+                            responseString = client.UploadString(builder.Uri, postBody);
+                        }
+
+                        try
+                        {
+                            QnAresponse = JsonConvert.DeserializeObject<QnAMakerResult>(responseString);
+                            responseMsg = QnAresponse.Answer.ToString();
+                        }
+                        catch
+                        {
+                            throw new Exception("Unable to deserialize QnA Maker response string.");
+                        }
+                    }
+
+                    // return our reply to the user
+                    Activity reply = activity.CreateReply(responseMsg);
+                    await connector.Conversations.ReplyToActivityAsync(reply);
+                }
+                context.Wait(ActivityReceivedAsync);
+            }
+        }
+    }
+}
